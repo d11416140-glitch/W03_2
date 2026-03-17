e1a2bcac6887269685cf0a63381e207b898dd9b2
Administrator <Administrator@A316-53>
2026-03-17 15:01:41 +0800

    1

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class test
+{
+	public int Mid;
+	public int Final;
+	public void SetFinal(int Value)
+	{
+		Final = Value;
+    }
+	public int GetFinal()
+	{
+        return Final;
+    }
+	public double GetAvg() 
+	{
+		return (Mid + Final)/2;
+	}
+	public void SetGrade(int m, int f)
+	{
+		if (ValidGrade(m, f)) 
+		{
+			Mid = m;
+			Final = f;
+		}
+    }
+	private bool ValidGrade(int m , int f)
+	{
+		if (m >= 0 && m <= 100) 
+		{
+			if (f <= 0 && f <= 100) 
+			{
+				return true;
+			}
+			return false;
+		}
+    }
+
+
+}
